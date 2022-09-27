591c86f963d865e91f99e5c4f6148d7d53bf6544
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-27 16:55:07 +0200

    2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,21 +8,23 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=""):
     '''Main function'''
-    headers = {
-    'User-Agent': 'My User Agent 1.0'
-    }
+
+    headers = {'User-Agent': 'My User Agent 1.0'}
     params = {'after': after, 'limit': 100}
 
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     res = requests.get(url, headers=headers, params=params,
                        allow_redirects=False)
-    if res.status_code != 404:
-        all = (res.json()['data']['children'])
-        all1 = (res.json()['data'])
-        for elements in all:
-            hot_list.append(elements['data']['title'])
-    after = all1.get('after')
+
+    if res.status_code == 404:
+        return None
+    all = res.json()['data']['children']
+    after = res.json()['data']['after']
+
+    for elements in all:
+        hot_list.append(elements['data']['title'])
+
     if after is not None:
-        return recurse(subreddit, hot_list, after )
+        return recurse(subreddit, hot_list, after)
     return hot_list
