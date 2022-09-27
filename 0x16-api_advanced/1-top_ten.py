d51b626af700e8237dcdd345b5a78679a6d2ca40
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-27 12:07:34 +0200

    all + chmod

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 '''function that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit. If an invalid
 subreddit is given, the function should return 0.
