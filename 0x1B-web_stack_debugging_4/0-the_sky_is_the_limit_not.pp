31b3443e83b5012f3f8ee016b1c68038206b6c91
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-10-06 09:58:18 +0200

    all

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,9 @@
 
 exec { 'update_nginx':
   command => 'sed -i s/15/2000/ /etc/default/nginx',
+  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', 'usr/local/bin',],
 }
 exec { 'restart_nginx':
   command => 'sudo service nginx restart',
+  path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', 'usr/local/bin',],
 }
